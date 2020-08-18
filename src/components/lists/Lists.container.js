98194cee28b13e2c890afd1f98c6d8722e775b91
Murat AKMAMMEDOV <muratx10@gmail.com>
2020-08-18 17:23:55 +0300

    refactor: remove useScroll and add listener inside the component

--- src/components/lists/Lists.container.js
+++ src/components/lists/Lists.container.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import { shallowEqual, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router';
 import { loadMoreData } from '../../store/actions/actionCreators';
-import useScroll from '../../utils/useScroll';
 import List from '../list';
 
 const Lists = () => {
   const state = useSelector(state => state, shallowEqual);
-  useScroll(loadMoreData(state.next));
+  const dispatch = useDispatch();
+
+  const listener = () => {
+    if (((window.innerHeight + window.scrollY) >= document.body.offsetHeight)) {
+      if (!state.isLoading && loadMoreData(state.next)) dispatch(loadMoreData(state.next));
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', listener)
+
+    return () => {
+      window.removeEventListener('scroll', listener)
+    }
+  })
 
   return (
     state.results.map(item => < List item={item} key={item.id}
